bba3caa74a2f57e36f7e11b9976863ef61d1ffac
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-15 00:08:29 +0530

    ADDED: Validation for phone number

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 public class UserRegistration {
     static Scanner input= new Scanner(System.in);
@@ -26,7 +25,7 @@ public class UserRegistration {
         System.out.println("Enter the last name: ");
         String lastName = input.nextLine();
 
-        //regex expression for last name
+        //regex expression for second name
         Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
         Matcher matcher = pattern.matcher(lastName);
         if (matcher.matches() == true)
@@ -35,10 +34,25 @@ public class UserRegistration {
             System.out.println("INVALID LAST NAME");
     }
 
+    //method for phone number
+    public static void phoneNumberValidation(){
+        String phoneNumber=input.nextLine();
+
+        //regex expression for phone number
+        Pattern pattern=Pattern.compile("^[0-9]{2}[: :][0-9]{10}$");
+        Matcher matcher= pattern.matcher(phoneNumber);
+        if (matcher.matches()==true)
+            System.out.println("Phone number is: "+phoneNumber);
+        else
+            System.out.println("INVALID PHONE NUMBER");
+
+    }
+
     //main method
     public static void main(String args[]){
         firstNameValidation();
         lastNameValidation();
+        phoneNumberValidation();
 
     }
 }
