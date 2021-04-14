167c30583e32153eb25e9afc03ff56c437a8ea3f
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-14 23:47:38 +0530

    ADDED: Validation for first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    static Scanner input= new Scanner(System.in);
+
+    //method for first name
+    public static void firstNameValidation() {
+
+            System.out.println("Enter the first name: ");
+            String firstName = input.nextLine();
+
+            //regex expression for first name
+            Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+            Matcher matcher = pattern.matcher(firstName);
+            if (matcher.matches() == true)
+                System.out.println("First name is: " + firstName);
+            else
+                System.out.println("INVALID NAME");
+        }
+
+    //main method
     public static void main(String args[]){
-	System.out.println("Welcome to the user registration");
-	}
+        firstNameValidation();
+    }
 }
